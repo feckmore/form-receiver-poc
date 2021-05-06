160032a82eb634c810d8098cdbe4da934e35cca5
David Schultz <dschultz@arthrex.com>
2021-05-06 13:34:35 -0500

    move form specific code

---
+++ form/form.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type FormWrapper struct {
+	*A `json:",omitempty"`
+	*B `json:",omitempty"`
+}
+
+type A struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+}
+
+type B struct {
+	ID        string `json:"id"`
+	Firstname string `json:"firstname"`
+}
+
+func (w *FormWrapper) UnmarshalJSON(data []byte) error {
+	log.Println("UnmarshalJSON()")
+	log.Println(string(data))
+
+	type Source struct {
+		SourceID string `json:"source"`
+	}
+
+	var source Source
+	err := json.Unmarshal(data, &source)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	out, _ := json.Marshal(source)
+	log.Println(string(out))
+
+	switch source.SourceID {
+	case "first":
+		log.Println("do first thing")
+		var a A
+		w.A = &a
+		return json.Unmarshal(data, &a)
+	case "second":
+		log.Println("do second thing")
+		var b B
+		w.B = &b
+		return json.Unmarshal(data, &b)
+	default:
+		log.Println("do default thing")
+	}
+
+	return nil
+}
--- form/main.go
+++ form/main.go
@@ -12,24 +12,9 @@ import (
 	"github.com/feckmore/form-receiver-poc/response"
 )
 
-type FormWrapper struct {
-	*A `json:",omitempty"`
-	*B `json:",omitempty"`
-}
-
-type A struct {
-	Name    string `json:"name"`
-	Address string `json:"address"`
-}
-
-type B struct {
-	ID        string `json:"id"`
-	Firstname string `json:"firstname"`
-}
-
 type Request events.APIGatewayProxyRequest
 
-// Handler is our lambda handler invoked by the `lambda.Start` function call
+// Handler is our lambda handler invoked by the `lambda.Start` function call in main()
 func Handler(ctx context.Context, request Request) (response.Response, error) {
 	log.Printf("%+v", request)
 
@@ -55,39 +40,4 @@ func Handler(ctx context.Context, request Request) (response.Response, error) {
 func main() {
 	log.SetFlags(log.Llongfile)
 	lambda.Start(Handler)
-}
-
-func (w *FormWrapper) UnmarshalJSON(data []byte) error {
-	log.Println("UnmarshalJSON()")
-	log.Println(string(data))
-
-	type Source struct {
-		SourceID string `json:"source"`
-	}
-
-	var source Source
-	err := json.Unmarshal(data, &source)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-	out, _ := json.Marshal(source)
-	log.Println(string(out))
-
-	switch source.SourceID {
-	case "first":
-		log.Println("do first thing")
-		var a A
-		w.A = &a
-		return json.Unmarshal(data, &a)
-	case "second":
-		log.Println("do second thing")
-		var b B
-		w.B = &b
-		return json.Unmarshal(data, &b)
-	default:
-		log.Println("do default thing")
-	}
-
-	return nil
 }
